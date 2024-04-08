d682dcdbc54f6f64588349eb1440d9b543a76b59
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-04-08 20:55:40 +0300

    pixabay api

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // body
 const bodyEl = document.querySelector('body');
 const themeEl = document.querySelector('.btn-theme');
@@ -57,4 +63,107 @@ function addLeadingZero(value) {
 
 startTimer();
 
-fetch('https://rickandmortyapi.com/api/character');
+// pixabay gallery
+
+const elements = {
+  searcForm: document.querySelector('#present_form'),
+  list: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader'),
+};
+
+hideLoader();
+elements.searcForm.addEventListener('submit', onSearchForm);
+
+function hideLoader() {
+  elements.loader.style.display = 'none';
+}
+
+function showLoader() {
+  elements.loader.style.display = 'block';
+}
+
+function onSearchForm(e) {
+  e.preventDefault();
+  const searchInput = e.currentTarget.elements.fieldSearch;
+  showLoader();
+  setTimeout(() => {
+    servicePixabay(searchInput.value)
+      .then(data => {
+        if (!data.hits.length) {
+          iziToast.show({
+            title: 'Error',
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+            position: 'topCenter',
+            backgroundColor: 'rgba(252, 249, 249, 1)',
+            messageColor: '#000',
+            zindex: '10000000',
+          });
+        }
+        elements.list.innerHTML = createMarkup(data.hits);
+        const galleryLibrary = new SimpleLightbox('.gallery a', {
+          captionDelay: 250,
+          captionsData: 'alt',
+        });
+        galleryLibrary.refresh();
+      })
+      .catch(err => console.log(err))
+      .finally(() => {
+        hideLoader();
+        e.target.reset();
+      });
+  }, 2000);
+}
+
+function servicePixabay(word) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '42170319-af092c1d236dd53a733e41db9';
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: word,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+
+  return fetch(`${BASE_URL}?${params}`).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
+}
+// вышиванка
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ downloads, views, likes, webformatURL, tags, largeImageURL }) =>
+        `<li class="gallery_item">
+        <a class="gallery_link" href="${largeImageURL}">
+          <img class="gallery_img" src="${webformatURL}" data-sourse="${largeImageURL}" alt="${tags}" />
+        </a>
+        <ul class="gallery_item_params">
+          <li class="gallery_item_params_link">
+            <svg class="gallery_svg" width="24" height="24">
+              <use href="../img/sprite.svg#icon-downloads"></use>
+            </svg>
+            ${downloads}
+          </li>
+          <li class="gallery_item_params_link">
+            <svg class="gallery_svg" width="24" height="24">
+              <use href="../img/sprite.svg#icon-heart"></use>
+            </svg>
+            ${likes}
+          </li>
+          <li class="gallery_item_params_link current">
+            <svg class="gallery_svg" width="24" height="24">
+              <use href="../img/sprite.svg#icon-search"></use>
+            </svg>
+            ${views}
+          </li>
+          </ul>
+          </li>
+          `
+    )
+    .join('');
+}
